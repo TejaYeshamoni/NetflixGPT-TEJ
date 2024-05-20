e0b71bc5550c1a9b3180d31fdf1424e42b3b2b05
TejaYeshamoni <yteja01@gmail.com>
2024-05-20 21:25:02 +0530

    updateB

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div><h1 className = "text-xl bg-gray-700 border-x-rose-500 rounded-3xl justify-end p-36 m-36">Nameste eye</h1></div>
+    <div><h1 className = "text-xl bg-gray-700 border-x-rose-500 rounded-3xl justify-end p-36 m-36">Nameste eye</h1>
+    <button>HII</button>
+    </div>
   );
 }
 
