24079aa3b31f33765e6e9942cc4cd844bf3f704c
TejaYeshamoni <yteja01@gmail.com>
2024-05-29 12:02:17 +0530

    publ

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,21 +1,23 @@
+
+
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const LOGO_ICON = `${BASE_URL}/logo1.png`;
-export const LOGO_RED = `${BASE_URL}/logo1.png`;
-export const LOGO_WHITE = `${BASE_URL}/logo1.png`;
-export const POSTER_BG = `${BASE_URL}/poster-bg.jpeg`;
-export const NO_POSTER = `${BASE_URL}/no_movie_poster.png`;
-export const BACKDROP = `${BASE_URL}/blur.png`;
-export const AVATAR_RED = "avatar-red.jpeg";
-export const AVATAR_BLUE = "avatar-blue.jpeg";
+export const LOGO_ICON = `${BASE_URL}/public/logo1.png`;
+export const LOGO_RED = `${BASE_URL}/public/logo1.png`;
+export const LOGO_WHITE = `${BASE_URL}/public/logo1.png`;
+export const POSTER_BG = `${BASE_URL}/public/poster-bg.jpeg`;
+export const NO_POSTER = `${BASE_URL}/public/no_movie_poster.png`;
+export const BACKDROP = `${BASE_URL}/public/blur.png`;
+export const AVATAR_RED = "public/avatar-red.jpeg";
+export const AVATAR_BLUE = "public/avatar-blue.jpeg";
 
 
 export const SCREENSHOT = {
-  "landing": `${BASE_URL}/ss-landing.png`,
-  "signin": `${BASE_URL}/ss-signin.png`,
-  "signup": `${BASE_URL}/ss-signup.png`,
-  "browse": `${BASE_URL}/ss-browsepage.png`,
-  "movieList": `${BASE_URL}/ss-movielist.png`,
-  "shimmer": `${BASE_URL}/ss-shimmer.png`,
-  "search": `${BASE_URL}/ss-gptsearchh.png`,
-  "watch": `${BASE_URL}/ss-watchpage.png`,
+  "landing": `${BASE_URL}/public/ss-landing.png`,
+  "signin": `${BASE_URL}/public/ss-signin.png`,
+  "signup": `${BASE_URL}/public/ss-signup.png`,
+  "browse": `${BASE_URL}/public/ss-browsepage.png`,
+  "movieList": `${BASE_URL}/public/ss-movielist.png`,
+  "shimmer": `${BASE_URL}/public/ss-shimmer.png`,
+  "search": `${BASE_URL}/public/ss-gptsearchh.png`,
+  "watch": `${BASE_URL}/public/ss-watchpage.png`,
 }
