3dfa5e7a1b0a0cbff8da906b536bc11e2d3afeb2
TejaYeshamoni <yteja01@gmail.com>
2024-05-29 17:43:56 +0530

    cP

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,25 +1,22 @@
 
 
-
-
-
-export const LOGO_ICON = `./logo1.png`;
-export const LOGO_RED = `./logo1.png`;
-export const LOGO_WHITE = `./logo1.png`;
-export const POSTER_BG = `./poster-bg.jpeg`;
-export const NO_POSTER = `./no_movie_poster.png`;
-export const BACKDROP = `./blur.png`;
-export const AVATAR_RED = "./avatar-red.jpeg";
-export const AVATAR_BLUE = "./avatar-blue.jpeg";
+export const LOGO_ICON = "logo1.png";
+export const LOGO_RED = "logo1.png";
+export const LOGO_WHITE = "logo1.png";
+export const POSTER_BG = "poster-bg.jpeg";
+export const NO_POSTER = "no_movie_poster.png";
+export const BACKDROP = "blur.png";
+export const AVATAR_RED = "avatar-red.jpeg";
+export const AVATAR_BLUE = "avatar-blue.jpeg";
 
 
 export const SCREENSHOT = {
-  "landing": `./ss-landing.png`,
-  "signin": `./ss-signin.png`,
-  "signup": `./ss-signup.png`,
-  "browse": `./ss-browsepage.png`,
-  "movieList": `./ss-movielist.png`,
-  "shimmer": `./ss-shimmer.png`,
-  "search": `./ss-gptsearchh.png`,
-  "watch": `./ss-watchpage.png`,
+  "landing": "ss-landing.png",
+  "signin": "ss-signin.png",
+  "signup": "ss-signup.png",
+  "browse": "ss-browsepage.png",
+  "movieList": "ss-movielist.png",
+  "shimmer": "ss-shimmer.png",
+  "search": "ss-gptsearchh.png",
+  "watch": "ss-watchpage.png",
 }
