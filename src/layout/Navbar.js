09e5ead01fbf0893f617739687b56350da15812e
TejaYeshamoni <yteja01@gmail.com>
2024-05-29 12:33:27 +0530

    changes on media

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { AVATAR_RED, LOGO_RED, BACKDROP, BASE_URL } from '../utils/constants';
+import { AVATAR_RED, LOGO_RED, BACKDROP } from '../utils/constants';
 import { PAGE } from '../router/routers';
 import { signOut } from 'firebase/auth';
 import { auth } from '../services/firebase';
@@ -116,7 +116,7 @@ const Navbar = () => {
             <div className="profile-dropdown relative" ref={dropdownRef}>
               <div className="flex items-center gap-3 cursor-pointer" onClick={handlerDropDown}>
                 <div className="thumb aspect-square w-8 h-8 bg-gray-800">
-                  <img src={` ${profilePhoto}`} alt={user.displayName} />
+                  <img src={`/${profilePhoto}`} alt={user.displayName} />
                 </div>
                 <div className="text-sm hidden lg:block">{user.displayName}</div>
               </div>
